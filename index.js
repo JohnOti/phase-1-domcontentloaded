39af25c357e96c4d0bf360522f6336b2b6689858
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-22 04:19:00 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){addContent()
+
+});
+function addContent(){
+  const newText=document.querySelector("#text");
+  newText.textContent="This is really cool!"
+}
